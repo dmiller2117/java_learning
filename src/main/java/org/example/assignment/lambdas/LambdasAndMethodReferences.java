7655ea77ab949d0bf87f37f6c23d2f577db7486c
David Miller <dmiller2117@gmail.com>
2023-11-29 22:18:41 +0000

    Unbound and constructor method reference assignment

--- src/main/java/org/example/assignment/lambdas/LambdasAndMethodReferences.java
+++ src/main/java/org/example/assignment/lambdas/LambdasAndMethodReferences.java
@@ -1,19 +1,21 @@
 package org.example.assignment.lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class LambdasAndMethodReferences {
 
     public static void main(String[] args) {
         staticMr();
         boundMr();
+        unboundMR();
+        constructorMR();
     }
 
-    static void staticMr() {
+    private static void staticMr() {
         System.out.println("Static Method Reference");
         List<Integer> integers = Arrays.asList(1, 2, 7, 5);
         integers.forEach(integer -> System.out.println("unsorted :: " + integer));
@@ -37,7 +39,7 @@ public class LambdasAndMethodReferences {
         integers.forEach(integer -> System.out.println("sorted :: " + integer));
     }
 
-    static void boundMr(){
+    private static void boundMr() {
         System.out.println("Bound Method Reference");
         String name = "Mr Joe Blogs";
         Predicate<String> startsWith = (prefix) -> name.startsWith(prefix);
@@ -47,4 +49,46 @@ public class LambdasAndMethodReferences {
         System.out.println(startsWithMr.test("Mr"));
         System.out.println(startsWithMr.test("Ms"));
     }
+
+    private static void unboundMR() {
+        System.out.println("Unbound Method Reference");
+        Predicate<String> isEmpty = s -> s.isEmpty();
+        System.out.println(isEmpty.test(""));
+        System.out.println(isEmpty.test("xyz"));
+        Predicate<String> isEmptyMr = String::isEmpty;
+        System.out.println(isEmptyMr.test(""));
+        System.out.println(isEmptyMr.test("xyz"));
+
+        BiPredicate<String, String> startsWithFirst = (aString, aPrefix) -> aString.startsWith(aPrefix);
+        System.out.println(startsWithFirst.test("Mr Joe Bloggs", "Mr"));
+        System.out.println(startsWithFirst.test("Mr Joe Bloggs", "Ms"));
+        BiPredicate<String, String> startsWithFirstMr = String::startsWith;
+        System.out.println(startsWithFirstMr.test("Mr Joe Bloggs", "Mr"));
+        System.out.println(startsWithFirstMr.test("Mr Joe Bloggs", "Ms"));
+    }
+
+
+    private static void constructorMR() {
+        System.out.println("Constructor Method Reference");
+        Supplier<List<String>> createArrayList = () -> new ArrayList<>();
+        List<String> stringList = createArrayList.get();
+        stringList.add("Lambda");
+        System.out.println(stringList);
+
+        Supplier<List<String>> createArrayListMr = ArrayList::new;
+        stringList = createArrayListMr.get();
+        stringList.add("Lambda MR");
+        System.out.println(stringList);
+
+        Function<Integer, List<String>> createListWithSize = integer -> new ArrayList<>(integer);
+        stringList = createListWithSize.apply(10);
+        stringList.add("Lambda with list size");
+        System.out.println(stringList);
+
+        Function<Integer, List<String>> createListWithSizeMr = ArrayList::new;
+        stringList = createListWithSizeMr.apply(10);
+        stringList.add("Lambda with list size and method reference");
+        System.out.println(stringList);
+    }
+
 }
